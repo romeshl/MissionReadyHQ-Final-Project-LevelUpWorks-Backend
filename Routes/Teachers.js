8761743f9cf1cff2beb44b7349d7f1a7cbcadcdb
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-05-29 13:34:39 +1200

    Re-wrote Students and Teachers routes with asnyc, await for better readability.

--- Routes/Teachers.js
+++ Routes/Teachers.js
@@ -1,66 +1,60 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 
-const dbConnection = require("../dbConnection");
+const db = require("../dbConnection").promise();
 
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body; // grabbing data from req.body
-
-  dbConnection.execute(
-    // runs the SQL query
-    "SELECT name, password  FROM teacher WHERE email = ?;",
-    [email],
-    (err, results) => {
-      if (err) {
-        // check for any errors generated while running the query
-        console.log("Error occurred", err);
-        return res.status(500).json({ unkown: "Unknown error" });
-      }
-      // If no results are found in the table
-      else if (results.length === 0) {
-        return res
-          .status(404)
-          .json({ error: "No user account with that e-mail address." });
-      }
-      // If at least one row in the table
-      else if (results.length >= 1) {
-        const hashedPass = results[0].password;
-        bcrypt.compare(password, hashedPass, (err, validPass) => {
-          // check if passwords match
-          if (validPass) {
-            return res.status(200).json(results[0].name);
-          } else {
-            // passwords don't match
-            return res.status(401).json({ error: "Incorrect password" });
-          }
-        });
-      }
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body; // grabbing data from req.body
+    const [results] = await db.execute(
+      "SELECT name, password  FROM teacher WHERE email = ?;",
+      [email]
+    );
+    if (results.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No user account with that e-mail address." });
+    } else if (results.length >= 1) {
+      const hashedPass = results[0].password;
+      bcrypt.compare(password, hashedPass, (err, validPass) => {
+        // check if passwords match
+        if (validPass) {
+          // if they match send 200 status and name
+          return res.status(200).json(results[0].name);
+        } else {
+          // passwords don't match
+          return res.status(401).json({ error: "Incorrect password" });
+        }
+      });
     }
-  );
+  } catch (err) {
+    console.log("Error occurred", err);
+    return res.status(500).json({ unknown: "Unknown error" });
+  }
 });
 
-router.post("/signup", (req, res) => {
-    const { name, email, password } = req.body;
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body; // grabbing data from req.body
     const hashedPass = bcrypt.hashSync(password, 10); // Hashes the password using bcrypt
 
     const queryString =
-        "INSERT INTO teacher (name, email, password) VALUES (?,?,?);";
+      "INSERT INTO teacher (name, email, password) VALUES (?,?,?);";
 
-    dbConnection.execute(queryString, [name, email, hashedPass], (err) => {
-        console.log(err);
-        if (err) {
-            if (err.code === "ER_DUP_ENTRY") {
-                return res.status(409).json({ error: "Email already exists." });
-            }
-            console.log("Error creating user", err);
-            return res.sendStatus(500);
-        } else {
-            return res.status(200).json({ message: "User created successfully" });
-        }
-    });
-})
+    const [results] = await db.execute(queryString, [name, email, hashedPass]);
+    console.log(results);
+    return res.status(200).json({ message: "User created successfully" });
+  } catch (err) {
+    if (err) {
+      if (err.code === "ER_DUP_ENTRY") {
+        // checks if the e-mail already exists in the database
+        return res.status(409).json({ error: "Email already exists." });
+      }
+      return res.sendStatus(500).json({ error: "Unknown error." });
+    }
+  }
+});
 
 module.exports = router;
-
