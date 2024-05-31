eb20bcaa49c487e59e5c1b81ce5873ad97b29fa7
Romesh Lankathilaka <romeshl@missionreadyhq.com>
2024-05-31 15:05:52 +1200

    Added HTML info for the backend landing (home page)

--- server.js
+++ server.js
@@ -19,8 +19,17 @@ app.use("/Teachers", teacherRoutes);
 
 
 app.get("/", (req, res) => {
-  res.status(200).json({ Test: `${process.env.MYSQL_USER}` });
-})
+  res.status(200)
+    .send(`<div style="border:2px solid silver; margin: 20px auto; padding: 30px; border-radius: 10px; text-align: center; background-color: gainsboro;">
+    <h1 style="font-family:tahoma; color: dimgray;">MissionReadyHQ-Final-Project-LevelUpWorks</h1>
+    <p style="font-family: tahoma">This is the backend of the LevelUp Works project.</p>
+    <p style="font-family: tahoma">Please use the following links to find the related resources.</p>
+    <a href="https://romeshl.github.io/MissionReadyHQ-Final-Project-LevelUpWorks-Frontend/" target="_blank" ><p style="font-family: tahoma">Frontend Home Page</p></a>
+    <a href="https://github.com/romeshl/MissionReadyHQ-Final-Project-LevelUpWorks-Frontend" target="_blank" ><p style="font-family: tahoma">Frontend Github repository</p></a>
+    <a href="https://github.com/romeshl/MissionReadyHQ-Final-Project-LevelUpWorks-Backend" target="_blank" ><p style="font-family: tahoma">Backend Github repository</p></a>
+    </div>`);
+});
+
 
 // Set API port and start listening
 const PORT = process.env.PORT;
